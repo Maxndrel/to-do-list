072b609041fdc19ebac3355fd731a7c8716df844
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-10 14:12:22 +0100

    v1.1

---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+      <div class="flex gap-[20px] flex-col items-center">
+            <div class="w-[250px] grow">
+                <h2>
+                    My Tasks
+                </h2>
+                <div>
+                    <h3>Todays Progress</h3>
+
+                </div>
+            </div>
+
+
+            <div class="w-[250px] grow">
+                <h3>All Tasks</h3>
+                <div>
+                    <p>Active</p>
+                    <p>Completed</p>
+                </div>
+            </div>
+
+
+            <div class="w-[250px] grow">
+                <h3>Priority</h3>
+                <div>
+                    <p>High</p>
+                    <p>Medium</p>
+                    <p>Low</p>
+                </div>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Sidebar
--- src/main app/main_page.jsx
+++ src/main app/main_page.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import Sidebar from '../component/Sidebar'
 
 const main_page = () => {
   return (
-    <div class="flex gap-[1%] flex-wrap content-stretch justify-center flex-col">
+    <div class="flex gap-[20px] flex-row items-center flex-nowrap h-full">
       {/* left side or side bar */}
-      <div class="w-[30%] h-full">
-      1
+      <div class="w-[30%] h-150 border-2">
+      < Sidebar />
       </div>
       
       {/* right side */}
-      <div class="container w-[70%] h-full">
-        <div class="item">Header</div>
+      <div class=" w-[70%] h-150 border-2">
+        <div class="item">
+          <div>
+            <h1>All Tasks</h1>
+            <p>Manage your tasks efficently</p>
+          </div>
+
+          <div>
+            <button class="border-2 rounded-md p-2 bg-blue-500 text-white">+ filter</button>
+          </div>
+
+          <div>
+            <button class="border-2 rounded-md p-2 bg-blue-500 text-white">+ filter</button>
+          </div>
+
+        </div>
         <div class="item">Content</div>
       
       </div>
