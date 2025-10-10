2ba1add203b22ebe48c4ef30d0f213e46b821c45
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-10 13:38:29 +0100

    sidebar

--- src/main app/main_page.jsx
+++ src/main app/main_page.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const main_page = () => {
   return (
-    <div class="flex gap-[2%] flex-wrap content-start">
-  <div class="w-full h-[5%]">Header</div>
-  <div class="w-1/4 h-3/4">Sidebar</div>
-  <div class="grow h-3/4">Content</div>
-</div>
+    <div class="flex gap-[1%] flex-wrap content-stretch justify-center flex-col">
+      {/* left side or side bar */}
+      <div class="w-[30%] h-full">
+      1
+      </div>
+      
+      {/* right side */}
+      <div class="container w-[70%] h-full">
+        <div class="item">Header</div>
+        <div class="item">Content</div>
+      
+      </div>
+    </div>
   )
 }
 
