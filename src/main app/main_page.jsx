8d8cd3c930f1158325ef9dfe70288a0d30f26bdc
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-13 00:52:20 +0100

    v1.3

--- src/main app/main_page.jsx
+++ src/main app/main_page.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import Sidebar from '../component/Sidebar'
+import { HugeiconsIcon } from '@hugeicons/react';
+import { SearchIcon, Add02Icon } from '@hugeicons/core-free-icons';
 
 const main_page = () => {
   return (
-    <div class="flex gap-[10px] flex-row items-center flex-nowrap h-full m-1">
+    <div className="flex gap-[10px] flex-row items-center flex-nowrap h-full m-1">
       {/* left side or side bar */}
-      <div class="w-[20%] h-150 border-2">
+      <div className="w-[20%] h-150 border-2">
       <Sidebar/>
       </div>
       
       {/* right side */}
-      <div class=" w-[80%] h-150 border-2 pr-10 pl-10">
-        <div class="item">
-          <div>
-            <h1>All Tasks</h1>
-            <p>Manage your tasks efficently</p>
+      <div className=" w-[80%] h-150 border-2 pr-15 pl-15">
+        <div className="item flex justify-between">
+          <div className='pt-6 pb-6'>
+            <h1 className='text-2xl font-semibold'>Today's Tasks</h1>
+            <p className='text-sm'>Manage your tasks efficently</p>
           </div>
 
-
+          <p className='pt-8 text-sm'>Friday, October 12, 2025 </p>
         </div>
-        <div class="item">
+
+        <div className="item">
           <div className='flex justify-center items-center bg-gray-100 p-4 rounded-lg'>
             <form action="#" method="get">
-              <input type="text" placeholder="Search task" class="border-2 rounded-md p-2 w-180"/>
-              <button type="submit" class="border-2 rounded-md p-2 bg-blue-500 text-white">Search</button>
+              <input type="text" placeholder="Search task" className="border-2 rounded-md p-2 w-180 relative"/>
+              <button type="submit" className=" rounded-xl p-2 text-white absolute right-60 top-31"><HugeiconsIcon icon={SearchIcon} size={24} color='grey'/></button>
             </form>
+            <button className='p-2.5 bg-sky-950 rounded-lg ml-2 text-white flex gap-1'> <HugeiconsIcon icon={Add02Icon} size={18} className='mt-0.5' /> Add Task</button>
           </div>
 
           
