8cee6cb2d924e7bf26f3537c7b752e597f539aa2
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-12 23:46:01 +0100

    complete sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -23,37 +23,47 @@ const Sidebar = () => {
             <div class="w-[250px] grow mb-2">
                 <h3 className='text-lg mb-2'>Categories</h3>
                 <div>
-                    <div className='flex gap-2 align-center'>
-                        <HugeiconsIcon
-                            icon={TaskDaily01Icon}
-                            size={18}
-                            className=''
-                        />
-                        <p className='text-md text-center mb-2'>
-                            All Tasks
-                        </p>
+                    <div className='flex justify-between mb-2'>
+                        <div className='flex gap-2 align-center'>
+                            <HugeiconsIcon
+                                icon={TaskDaily01Icon}
+                                size={18}
+                                className=''
+                            />
+                            <p className='text-md text-center'>
+                                All Tasks
+                            </p>
+                        </div>
+                        <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>20</p>
                     </div>
 
-                    <div className='flex gap-2 align-center'>
-                        <HugeiconsIcon
-                            icon={Clock01Icon}
-                            size={18}
-                            className=''
-                        />
-                        <p className='text-md text-center mb-2'>
-                            Active
-                        </p>
+                    <div className='flex justify-between mb-2'>
+                        <div className='flex gap-2 align-center'>
+                            <HugeiconsIcon
+                                icon={Clock01Icon}
+                                size={18}
+                                className=''
+                            />
+                            <p className='text-md text-center mb-2'>
+                                Active
+                            </p>
+                        </div>
+                        <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>0</p>
                     </div>
 
-                    <div className='flex gap-2 align-center'>
-                        <HugeiconsIcon
-                            icon={PhoneCheckIcon}
-                            size={18}
-                            className=''
-                        />
-                        <p className='text-md text-center'>
-                            Completed
-                        </p>
+                    <div className='flex justify-between'>
+                        <div className='flex gap-2 align-center'>
+                            <HugeiconsIcon
+                                icon={PhoneCheckIcon}
+                                size={18}
+                                className=''
+                            />
+                            <p className='text-md text-center'>
+                                Completed
+                            </p>
+                        </div>
+
+                        <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>20</p>
                     </div>
                 </div>
             </div>
@@ -62,19 +72,28 @@ const Sidebar = () => {
             <div class="w-[250px] grow">
                 <h3 className='text-lg mb-2'>Priority</h3>
                 <div>
-                    <div className='flex gap-2 align-center'>
-                        <div className='w-[8px] h-[8px] rounded-xl bg-red-500 mt-1.5'/>
-                        <p className='text-base mb-2'>High</p>
+                    <div className='flex justify-between mb-2'>
+                        <div className='flex gap-2 align-center'>
+                            <div className='w-[8px] h-[8px] rounded-xl bg-red-500 mt-1.5'/>
+                            <p className='text-base mb-2'>High</p>
+                        </div>
+                         <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>10</p>                   
                     </div>
 
-                    <div className='flex gap-2 align-center'>
-                        <div className='w-[8px] h-[8px] rounded-xl bg-yellow-500 mt-1.5'/>
-                        <p className='text-base mb-2'>Medium</p>
+                    <div className='flex justify-between mb-2'>
+                        <div className='flex gap-2 align-center'>
+                            <div className='w-[8px] h-[8px] rounded-xl bg-yellow-500 mt-1.5'/>
+                            <p className='text-base mb-2'>Medium</p>
+                        </div>
+                          <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>2</p>                  
                     </div>
 
+                    <div className='flex justify-between'>
                     <div className='flex gap-2 align-center'>
                         <div className='w-[8px] h-[8px] rounded-xl bg-blue-500 mt-1.5'/>
                         <p className='text-base'>Low</p>
+                    </div>
+                         <p className='p-1 w-[30px] rounded-2xl bg-gray-200 text-base text-center'>8</p>                   
                     </div>
                 </div>
 
