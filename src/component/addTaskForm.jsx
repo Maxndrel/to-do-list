e9392ff407e59e2ba97dfa64cbe97a98a99ce2fe
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-13 01:39:47 +0100

    v1.4

--- src/component/addTaskForm.jsx
+++ src/component/addTaskForm.jsx
@@ -11,7 +11,36 @@ const addTaskForm = () => {
       }
   return (
     <div>
-       
+       <div className='flex justify-center items-center'>
+            <form className='flex flex-col gap-4 mt-10 w-1/2'>
+              <input 
+              type="text" 
+              placeholder="task header" 
+              class="border-2 rounded-md p-2 w-100"/>
+
+              <input 
+              type="text" 
+              placeholder="Describe task" 
+              class="border-2 rounded-md p-2 w-100"/>
+
+              <select
+              className="border rounded-lg p-2 w-40"
+              >
+                <option value="">- Priority Level -</option>
+                <option>Low</option>
+                <option>Medium</option>
+                <option>High</option>
+              </select>
+
+              <input 
+              type="datetime-local" 
+              name="" 
+              id="" />
+
+              <button type="submit" class="border-2 rounded-lg p-2 bg-sky-950 hover:bg-sky-900 text-white ">Add Task</button>
+            </form>
+        </div>
+
     </div>
   )
 }
--- src/main app/main_page.jsx
+++ src/main app/main_page.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../component/Sidebar'
 import { HugeiconsIcon } from '@hugeicons/react';
 import { SearchIcon, Add02Icon } from '@hugeicons/core-free-icons';
 import { useState } from 'react';
+import AddTaskForm from '../component/addTaskForm';
 
 const main_page = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,7 +33,7 @@ const main_page = () => {
               <input type="text" placeholder="Search task" className="border-2 rounded-md p-2 w-180 relative"/>
               <button type="submit" className=" rounded-xl p-2 text-white absolute right-60 top-31"><HugeiconsIcon icon={SearchIcon} size={24} color='grey'/></button>
             </form>
-            <button className='p-2.5 bg-sky-950 rounded-lg ml-2 text-white flex gap-1' onClick={() => setIsOpen(true)}> <HugeiconsIcon icon={Add02Icon} size={18} className='mt-0.5' /> Add Task</button>
+            <button className='p-2.5 bg-sky-950 hover:bg-sky-900 rounded-lg ml-2 text-white flex gap-1' onClick={() => setIsOpen(true)}> <HugeiconsIcon icon={Add02Icon} size={18} className='mt-0.5' /> Add Task</button>
           </div>
 
           {isOpen && (
@@ -42,62 +43,13 @@ const main_page = () => {
                 <h2 className="text-xl font-semibold mb-4">Add New Task</h2>
 
                 {/* Form */}
-                <form onSubmit={handleSubmit}>
-                  <input
-                    type="text"
-                    placeholder="Enter task..."
-                    value={task}
-                    onChange={(e) => setTask(e.target.value)}
-                    required
-                    className="border border-gray-300 rounded-md w-full p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-green-500"
-                  />
-                  <div className="flex justify-end gap-2">
-                    <button
-                      type="button"
-                      onClick={() => setIsOpen(false)}
-                      className="bg-gray-300 text-gray-800 px-3 py-1 rounded-md hover:bg-gray-400"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="submit"
-                      className="bg-green-600 text-white px-3 py-1 rounded-md hover:bg-green-700"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </form>
+                <AddTaskForm />
               </div>
             </div>
           )}
 
           
         </div>
-      <div className='flex justify-center items-center'>
-            <form className='flex flex-col gap-4 mt-10 w-1/2'>
-              <input 
-              type="text" 
-              placeholder="task header" 
-              class="border-2 rounded-md p-2 w-100"/>
-
-              <input 
-              type="text" 
-              placeholder="Describe task" 
-              class="border-2 rounded-md p-2 w-100"/>
-
-              <select
-              className="border rounded-lg p-2 w-40"
-              >
-                <option value="">- Priority Level -</option>
-                <option>Low</option>
-                <option>Medium</option>
-                <option>High</option>
-              </select>
-
-              <button type="submit" class="border-2 rounded-md p-2 bg-sky-950 text-white">Add Task</button>
-            </form>
-        </div>
-
 
       </div>
     </div>
