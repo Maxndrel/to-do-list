d81e93966bd643dd09a86d1993ef79859d55ad42
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-10-13 08:56:25 +0100

    task display

--- src/component/addTaskForm.jsx
+++ src/component/addTaskForm.jsx
@@ -1,95 +1,12 @@
 import React from 'react'
 import { useState } from 'react';
-import { SearchIcon, Add02Icon } from '@hugeicons/core-free-icons';
-import { HugeiconsIcon } from '@hugeicons/react';
+
 
 const addTaskForm = () => {
-    const [isOpen, setIsOpen] = useState(false);
-  const [task, setTask] = useState("");
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Task Added: ${task}`);
-        setTask("");
-        setIsOpen(false);
-      }
     
   return (
     <div>
-       <div className='flex justify-center items-center bg-gray-100 p-4 rounded-lg'>
-            <form action="#" method="get">
-              <input type="text" placeholder="Search task" className="border-2 rounded-md p-2 w-180 relative"/>
-              <button type="submit" className=" rounded-xl p-2 text-white absolute right-60 top-31"><HugeiconsIcon icon={SearchIcon} size={24} color='grey'/></button>
-            </form>
-            <button className='p-2.5 bg-sky-950 hover:bg-sky-900 rounded-lg ml-2 text-white flex gap-1' onClick={() => setIsOpen(true)}> <HugeiconsIcon icon={Add02Icon} size={18} className='mt-0.5' /> Add Task</button>
-          </div>
-
-          {isOpen && (
-            <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
-              {/* Popup Box */}
-              <div className="bg-white rounded-lg p-6 w-180 shadow-lg relative">
-                <h2 className="text-xl text-center font-semibold mb-2">Add New Task</h2>
-
-                {/* Form */}
-                <div className='flex justify-center items-center'>
-                    <form  onSubmit={handleSubmit} className='flex flex-col gap-4 mt-5 w-1/2'>
-                      <input 
-                      type="text" 
-                      placeholder="task header"
-                      value={task}
-                      onChange={(e) => setTask(e.target.value)}
-                      required 
-                      class="border-2 rounded-md p-2 w-100"/>
-
-                      <input 
-                      type="text" 
-                      placeholder="Describe task" 
-                      value={task}
-                      onChange={(e) => setTask(e.target.value)}
-                      required 
-                      class="border-2 rounded-md p-2 w-100"/>
-
-                      <select
-                      value={task}
-                      onChange={(e) => setTask(e.target.value)}
-                      required
-                      className="border rounded-lg p-2 w-40"
-                      >
-                        <option value="">- Priority Level -</option>
-                        <option>Low</option>
-                        <option>Medium</option>
-                        <option>High</option>
-                      </select>
-
-                      <div className='text-base flex justify-between'>
-                        <label>Set Deadline: </label>
-                        <input 
-                        type="datetime-local" 
-                        value={task}
-                        onChange={(e) => setTask(e.target.value)} />
-                      </div>
-                    
-                      <div className='flex gap-2'>
-                        <button 
-                        type="submit" 
-                        class="border-2 rounded-lg p-2 bg-sky-950 hover:bg-sky-900 text-white ">
-                          Add Task
-                        </button>
-
-                        <button
-                          type="button"
-                          onClick={() => setIsOpen(false)}
-                          className="bg-red-600 text-gray-800 px-3 py-1 rounded-md hover:bg-red-500"
-                        > 
-                          Cancel
-                        </button>
-                      </div>
-                    </form>
-                </div>
-              </div>
-            </div>
-          )}
-
+       
     </div>
   )
 }
--- src/main app/main_page.jsx
+++ src/main app/main_page.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import Sidebar from '../component/Sidebar'
-import AddTaskForm from '../component/addTaskForm'
+import { SearchIcon, Add02Icon } from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react';
+import { useState } from 'react';
 
 const main_page = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [task, setTask] = useState([""]);
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Task Added: ${task}`);
+    setTask("");
+    setIsOpen(false);
+  }
+  
+  const [tasks, setTasks] = useState([]); // stores all tasks
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [dueDateTime, setDueDateTime] = useState("");
+
+  // function to add a new task
+  const addTask = () => {
+    const newTask = {
+      title,
+      description,
+      priority,
+      dueDate,
+      dueDateTime,
+    };
+
+    setTasks([...tasks, newTask]); // add new task to the list
+
+    // clear inputs
+    setTitle("");
+    setDescription("");
+    setPriority("");
+    setDueDateTime("");
+  };
   
 
   return (
@@ -24,9 +60,94 @@ const main_page = () => {
         </div>
 
         <div className="item">
-          <AddTaskForm/>
+          <div className='flex justify-center items-center bg-gray-100 p-4 rounded-lg'>
+            <form action="#" method="get">
+              <input type="text" placeholder="Search task" className="border-2 rounded-md p-2 w-180 relative"/>
+              <button type="submit" className=" rounded-xl p-2 text-white absolute right-60 top-31"><HugeiconsIcon icon={SearchIcon} size={24} color='grey'/></button>
+            </form>
+            <button className='p-2.5 bg-sky-950 hover:bg-sky-900 rounded-lg ml-2 text-white flex gap-1' onClick={() => setIsOpen(true)}> <HugeiconsIcon icon={Add02Icon} size={18} className='mt-0.5' /> Add Task</button>
+          </div>
+
+          {isOpen && (
+            <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
+              {/* Popup Box */}
+              <div className="bg-white rounded-lg p-6 w-180 shadow-lg relative">
+                <h2 className="text-xl text-center font-semibold mb-2">Add New Task</h2>
+
+                {/* Form */}
+                <div className='flex justify-center items-center'>
+                    <form  onSubmit={handleSubmit} className='flex flex-col gap-4 mt-5 w-1/2'>
+                      <input 
+                      type="text" 
+                      placeholder="task title"
+                      value={title}
+                      onChange={(e) => setDescription(e.target.value)}
+                      required 
+                      class="border-2 rounded-md p-2 w-100"/>
+
+                      <input 
+                      type="text" 
+                      placeholder="Describe task" 
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      required 
+                      class="border-2 rounded-md p-2 w-100"/>
+
+                      <select
+                      value={priority}
+                      onChange={(e) => setPriority(e.target.value)}
+                      required
+                      className="border rounded-lg p-2 w-40"
+                      >
+                        <option value="">- Priority Level -</option>
+                        <option>Low</option>
+                        <option>Medium</option>
+                        <option>High</option>
+                      </select>
+
+                      <div className='text-base flex justify-between'>
+                        <label>Set Deadline: </label>
+                        <input 
+                        type="datetime-local" 
+                        value={dueDateTime}
+                        onChange={(e) => setTask(e.target.value)} />
+                      </div>
+                    
+                      <div className='flex gap-2'>
+                        <button 
+                        type="submit" 
+                        class="border-2 rounded-lg p-2 bg-sky-950 hover:bg-sky-900 text-white ">
+                          Add Task
+                        </button>
+
+                        <button
+                          type="button"
+                          onClick={() => setIsOpen(false)}
+                          className="bg-red-600 text-gray-800 px-3 py-1 rounded-md hover:bg-red-500"
+                        > 
+                          Cancel
+                        </button>
+                      </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+          )}
+
         </div>
 
+        <div>
+          {/* task list */}
+          <div className='mt-10'>
+            <ul>
+              {task.map((task, index) => (
+                <li key={index}>
+                  {task}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
